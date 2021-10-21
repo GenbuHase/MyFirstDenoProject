4a6a5ea70a3127b87f394abf898cd97f913aa9df
Genbu Hase <14960841+GenbuHase@users.noreply.github.com>
2021-10-22 04:09:57 +0900

    2021/10/22 + 9-006: "An Implementation of The Unix Cat Program"を実装

---
+++ src/9-006_An-Implementation-of-The-Unix-Cat-Program/main.ts
@@ -0,0 +1,8 @@
+import { copy } from "https://deno.land/std@0.112.0/io/util.ts";
+
+for (const filename of Deno.args) {
+	const file = await Deno.open(filename);
+	
+	await copy(file, Deno.stdout);
+	file.close();
+}
