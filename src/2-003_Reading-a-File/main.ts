6b6044d55df7afcc2c9d42977a6c625bf0b4e18a
Genbu Hase <14960841+GenbuHase@users.noreply.github.com>
2021-10-22 04:06:14 +0900

    2021/10/22 + 2-003: "Reading a File"を実装

---
+++ src/2-003_Reading-a-File/main.ts
@@ -0,0 +1,9 @@
+import { copy } from "https://deno.land/std@0.112.0/io/util.ts";
+
+const filenames = Deno.args;
+for (const filename of filenames) {
+	const file = await Deno.open(filename);
+	
+	await copy(file, Deno.stdout);
+	file.close();
+}
