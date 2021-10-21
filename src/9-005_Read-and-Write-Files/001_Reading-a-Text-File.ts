82511f61b586267cd25ecd640d823c6522c8c98e
Genbu Hase <14960841+GenbuHase@users.noreply.github.com>
2021-10-22 04:09:17 +0900

    2021/10/22 + 9-005: "Read and Write Files"を実装

---
+++ src/9-005_Read-and-Write-Files/001_Reading-a-Text-File.ts
@@ -0,0 +1,2 @@
+const text = await Deno.readTextFile("./src/9-005_Read-and-Write-Files/input.json");
+console.log(text);
---
+++ src/9-005_Read-and-Write-Files/002_Writing-a-Text-File.ts
@@ -0,0 +1,2 @@
+await Deno.writeTextFile("./src/9-005_Read-and-Write-Files/output.txt", "Hello, World!");
+console.log("Written to ./output.txt");
